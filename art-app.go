acfe8479e659f843ab21842f3a84d5c97fb2fdfa
Winnie Wong <winwong03@gmail.com>
2018-02-06 15:12:52 -0800

    starter code

---
+++ art-app.go
@@ -0,0 +1,65 @@
+/*
+
+A trivial application to illustrate how the blockartlib library can be
+used from an application in project 1 for UBC CS 416 2017W2.
+
+Usage:
+go run art-app.go
+*/
+
+package main
+
+// Expects blockartlib.go to be in the ./blockartlib/ dir, relative to
+// this art-app.go file
+import "./blockartlib"
+
+import "fmt"
+import "os"
+
+func main() {
+	minerAddr := "127.0.0.1:8080"
+	privKey := stub // TODO: use crypto/ecdsa to read pub/priv keys from a file argument.
+
+	// Open a canvas.
+	canvas, settings, err := blockartlib.OpenCanvas(minerAddr, privKey)
+	if checkError(err) != nil {
+		return
+	}
+
+	validateNum := 2
+
+	// Add a line.
+	shapeHash, blockHash, ink, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 0 0 L 0 5", "transparent", "red")
+	if checkError(err) != nil {
+		return
+	}
+
+	// Add another line.
+	shapeHash2, blockHash2, ink2, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 0 0 L 5 0", "transparent", "blue")
+	if checkError(err) != nil {
+		return
+	}
+
+	// Delete the first line.
+	ink3, err := canvas.DeleteShape(validateNum, shapeHash)
+	if checkError(err) != nil {
+		return
+	}
+
+	// assert ink3 > ink2
+
+	// Close the canvas.
+	ink4, err := canvas.CloseCanvas()
+	if checkError(err) != nil {
+		return
+	}
+}
+
+// If error is non-nil, print it out and return it.
+func checkError(err error) error {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error ", err.Error())
+		return err
+	}
+	return nil
+}
---
+++ blockartlib/blockartlib.go
@@ -0,0 +1,199 @@
+/*
+
+This package specifies the application's interface to the the BlockArt
+library (blockartlib) to be used in project 1 of UBC CS 416 2017W2.
+
+*/
+
+package blockartlib
+
+import "crypto/ecdsa"
+import "fmt"
+
+// Represents a type of shape in the BlockArt system.
+type ShapeType int
+
+const (
+	// Path shape.
+	PATH ShapeType = iota
+
+	// Circle shape (extra credit).
+	// CIRCLE
+)
+
+// Settings for a canvas in BlockArt.
+type CanvasSettings struct {
+	// Canvas dimensions
+	CanvasXMax uint32
+	CanvasYMax uint32
+}
+
+// Settings for an instance of the BlockArt project/network.
+type MinerNetSettings struct {
+	// Hash of the very first (empty) block in the chain.
+	GenesisBlockHash string
+
+	// The minimum number of ink miners that an ink miner should be
+	// connected to. If the ink miner dips below this number, then
+	// they have to retrieve more nodes from the server using
+	// GetNodes().
+	MinNumMinerConnections uint8
+
+	// Mining ink reward per op and no-op blocks (>= 1)
+	InkPerOpBlock   uint32
+	InkPerNoOpBlock uint32
+
+	// Number of milliseconds between heartbeat messages to the server.
+	HeartBeat uint32
+
+	// Proof of work difficulty: number of zeroes in prefix (>=0)
+	PoWDifficultyOpBlock   uint8
+	PoWDifficultyNoOpBlock uint8
+
+	// Canvas settings
+	CanvasSettings CanvasSettings
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// <ERROR DEFINITIONS>
+
+// These type definitions allow the application to explicitly check
+// for the kind of error that occurred. Each API call below lists the
+// errors that it is allowed to raise.
+//
+// Also see:
+// https://blog.golang.org/error-handling-and-go
+// https://blog.golang.org/errors-are-values
+
+// Contains address IP:port that art node cannot connect to.
+type DisconnectedError string
+
+func (e DisconnectedError) Error() string {
+	return fmt.Sprintf("BlockArt: cannot connect to [%s]", string(e))
+}
+
+// Contains amount of ink remaining.
+type InsufficientInkError uint32
+
+func (e InsufficientInkError) Error() string {
+	return fmt.Sprintf("BlockArt: Not enough ink to addShape [%d]", uint32(e))
+}
+
+// Contains the offending svg string.
+type InvalidShapeSvgStringError string
+
+func (e InvalidShapeSvgStringError) Error() string {
+	return fmt.Sprintf("BlockArt: Bad shape svg string [%s]", string(e))
+}
+
+// Contains the offending svg string.
+type ShapeSvgStringTooLongError string
+
+func (e ShapeSvgStringTooLongError) Error() string {
+	return fmt.Sprintf("BlockArt: Shape svg string too long [%s]", string(e))
+}
+
+// Contains the bad shape hash string.
+type InvalidShapeHashError string
+
+func (e InvalidShapeHashError) Error() string {
+	return fmt.Sprintf("BlockArt: Invalid shape hash [%s]", string(e))
+}
+
+// Contains the bad shape hash string.
+type ShapeOwnerError string
+
+func (e ShapeOwnerError) Error() string {
+	return fmt.Sprintf("BlockArt: Shape owned by someone else [%s]", string(e))
+}
+
+// Empty
+type OutOfBoundsError struct{}
+
+func (e OutOfBoundsError) Error() string {
+	return fmt.Sprintf("BlockArt: Shape is outside the bounds of the canvas")
+}
+
+// Contains the hash of the shape that this shape overlaps with.
+type ShapeOverlapError string
+
+func (e ShapeOverlapError) Error() string {
+	return fmt.Sprintf("BlockArt: Shape overlaps with a previously added shape [%s]", string(e))
+}
+
+// Contains the invalid block hash.
+type InvalidBlockHashError string
+
+func (e InvalidBlockHashError) Error() string {
+	return fmt.Sprintf("BlockArt: Invalid block hash [%s]", string(e))
+}
+
+// </ERROR DEFINITIONS>
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Represents a canvas in the system.
+type Canvas interface {
+	// Adds a new shape to the canvas.
+	// Can return the following errors:
+	// - DisconnectedError
+	// - InsufficientInkError
+	// - InvalidShapeSvgStringError
+	// - ShapeSvgStringTooLongError
+	// - ShapeOverlapError
+	// - OutOfBoundsError
+	AddShape(validateNum uint8, shapeType ShapeType, shapeSvgString string, fill string, stroke string) (shapeHash string, blockHash string, inkRemaining uint32, err error)
+
+	// Returns the encoding of the shape as an svg string.
+	// Can return the following errors:
+	// - DisconnectedError
+	// - InvalidShapeHashError
+	GetSvgString(shapeHash string) (svgString string, err error)
+
+	// Returns the amount of ink currently available.
+	// Can return the following errors:
+	// - DisconnectedError
+	GetInk() (inkRemaining uint32, err error)
+
+	// Removes a shape from the canvas.
+	// Can return the following errors:
+	// - DisconnectedError
+	// - ShapeOwnerError
+	DeleteShape(validateNum uint8, shapeHash string) (inkRemaining uint32, err error)
+
+	// Retrieves hashes contained by a specific block.
+	// Can return the following errors:
+	// - DisconnectedError
+	// - InvalidBlockHashError
+	GetShapes(blockHash string) (shapeHashes []string, err error)
+
+	// Returns the block hash of the genesis block.
+	// Can return the following errors:
+	// - DisconnectedError
+	GetGenesisBlock() (blockHash string, err error)
+
+	// Retrieves the children blocks of the block identified by blockHash.
+	// Can return the following errors:
+	// - DisconnectedError
+	// - InvalidBlockHashError
+	GetChildren(blockHash string) (blockHashes []string, err error)
+
+	// Closes the canvas/connection to the BlockArt network.
+	// - DisconnectedError
+	CloseCanvas() (inkRemaining uint32, err error)
+}
+
+// The constructor for a new Canvas object instance. Takes the miner's
+// IP:port address string and a public-private key pair (ecdsa private
+// key type contains the public key). Returns a Canvas instance that
+// can be used for all future interactions with blockartlib.
+//
+// The returned Canvas instance is a singleton: an application is
+// expected to interact with just one Canvas instance at a time.
+//
+// Can return the following errors:
+// - DisconnectedError
+func OpenCanvas(minerAddr string, privKey ecdsa.PrivateKey) (canvas Canvas, setting CanvasSettings, err error) {
+	// TODO
+	// For now return DisconnectedError
+	return nil, CanvasSettings{}, DisconnectedError("")
+}
