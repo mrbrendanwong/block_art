16ba821220f8b5743274240df60bb07a88664b86
mrbrendanwong <brendonydwong@gmail.com>
2018-02-08 03:16:20 -0800

    Added initial miner2miner communication code

--- ink-miner.go
+++ ink-miner.go
@@ -15,6 +15,7 @@ import (
 	"encoding/gob"
 	"crypto/elliptic"
 	"time"
+	"sync"
 
 	"./blockartlib"
 	"log"
@@ -23,12 +24,17 @@ import (
 type InkMiner int
 
 var(
+	// Identity related variables
 	Server 			*rpc.Client				/* Connection to Server */
 	PubKey			ecdsa.PublicKey			/* Public and private key pair for validation */
 	PrivKey			*ecdsa.PrivateKey
+	LocalAddr 		net.Addr
 
+	// Error logging
 	errLog          *log.Logger = log.New(os.Stderr, "[serv] ", log.Lshortfile|log.LUTC|log.Lmicroseconds)
 
+	// Connected mineres
+	connectedMiners ConnectedMiners = ConnectedMiners{miners: make(map[string]*Miner)}
 )
 
 
@@ -39,13 +45,14 @@ func main(){
 	// check for correct arguments
 	args := os.Args
 
-	if len(args) != 4 {
-		fmt.Println("Usage: go run ink-miner.go [server ip:port] [pubKey] [privKey]")
+	if len(args) != 5 {
+		fmt.Println("Usage: go run ink-miner.go [server ip:port] [local ip:port] [pubKey] [privKey]")
 		return
 	}
 	serverAddr := args[1]
-	ln, _ := net.Listen("tcp", ":0")
-	localAddr := ln.Addr()
+	localAddr := args[2]
+	ln, _ := net.Listen("tcp", localAddr)
+	LocalAddr = ln.Addr()
 
 	// TODO:
 	// Generating public and private key pairing for now
@@ -66,7 +73,7 @@ func main(){
 
 	// Register miner on server
 	var settings MinerNetSettings
-	err = Server.Call("RServer.Register", MinerInfo{localAddr, PubKey}, &settings)
+	err = Server.Call("RServer.Register", MinerInfo{LocalAddr, PubKey}, &settings)
 	if err != nil{
 		fmt.Println(err)
 		return
@@ -75,8 +82,11 @@ func main(){
 	// start sending heartbeats
 	go sendHeartBeats()
 
+	// Get nodes from server and attempt to connect to them
+	GetNodes()
+
 	// TODO:
-	// Listen for incoming art node connections
+	// Listen for incoming miner connections
 	Inkminer := new(InkMiner)
 	miner := rpc.NewServer()
 	miner.Register(Inkminer)
@@ -167,4 +177,170 @@ func handleErrorFatal(msg string, e error) {
 	if e != nil {
 		errLog.Fatalf("%s, err = %s\n", msg, e.Error())
 	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CALLS TO SERVER
+////////////////////////////////////////////////////////////////////////////////
+
+/* Retrieves a list of new miner addresses from the server
+ * Attemps to connect to the new miners it retrieves
+ */
+func GetNodes() (err error) {
+	// Array of miner addresses to be returned by the server
+	var addrSet []net.Addr
+
+	fmt.Println("Checking for new miners")
+	err = Server.Call("RServer.GetNodes", PubKey, &addrSet)
+	if err != nil {
+		fmt.Println("Error getting nodes from server.")
+		return err
+	}
+
+	newMiners := 0
+
+	for _, addr := range addrSet {
+		if addr != LocalAddr {
+			fmt.Println("Connecting to miner...")
+			// Attempt to establish connections to retrieved miners
+			err = ConnectMiner(addr)
+			if err != nil {
+				fmt.Println("Could not connect to miner")
+			} else {
+				newMiners += 1
+			}
+		} else {
+			continue
+		}
+	}
+
+	fmt.Printf("Connected to %d new ink miners\n", newMiners)
+
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CALLS TO MINERS
+////////////////////////////////////////////////////////////////////////////////
+// Represents a miner. Adapted from server.go
+type Miner struct {
+	Address 			net.Addr
+	RecentHeartbeat 	int64
+	MinerConn 			*rpc.Client
+}
+
+// Map of all miners currently connected
+type ConnectedMiners struct {
+	sync.RWMutex
+	miners map[string]*Miner
+}
+
+// Establish RPC connection to other miners
+func ConnectMiner(addr net.Addr) (err error) {
+	//minerNetwork := addr.Network()
+	minerAddr := addr.String()
+	minerConn, err := rpc.Dial("tcp", minerAddr)
+	if err != nil {
+		fmt.Printf("Could not reach other miner at %s", minerAddr)
+		return err
+	}
+
+	args := &MinerInfo{Address: LocalAddr, Key: PubKey}
+	reply := MinerInfo{}
+	err = minerConn.Call("InkMiner.RegisterMiner", args, &reply)
+	if err != nil {
+		fmt.Println("Could not intiate intial RPC call to miner")
+		return err
+	}
+
+	fmt.Println("Connected to miner")
+
+	fmt.Println("Boom")
+	minerPubKey := reply.Key
+	minerPK := pubKeyToString(minerPubKey)
+
+	fmt.Println("Boom")
+	// Register miner to miner map
+	connectedMiners.Lock()
+	connectedMiners.miners[minerPK] = &Miner{
+		Address: addr,
+		RecentHeartbeat: time.Now().UnixNano(),
+		MinerConn: minerConn}
+	connectedMiners.Unlock()
+
+	fmt.Println("Registered fellow miner")
+
+	// TODO Start sending heartbeat to miner
+
+	return nil
+}
+
+func (m InkMiner) RegisterMiner(args *MinerInfo, reply *MinerInfo) (err error) {
+	minerAddr := args.Address
+	minerPubKey := args.Key
+	minerPK := pubKeyToString(minerPubKey)
+
+	fmt.Printf("Miner with address %s trying to connect\n", minerAddr.String(), minerPK)
+	*reply = MinerInfo{Address: LocalAddr, Key: PubKey}
+	fmt.Println("Miner has been connected")
+
+	// TODO
+	// Needs to dial back to other miner? (?)
+	// Needs UDP port of upd listener to send heartbeats back (?)
+
+	return nil
+}
+
+// Sends heartbeat signals to other miners
+func SendMinerHeartbeat() (err error) {
+	//TODO
+	return nil
+}
+
+// Listens for hearbeat signals from other mienrs
+func ListenMinerHeartbeat() (err error) {
+	//TODO
+	return nil
+}
+
+// Updates heartbeats for miners. Adapted from server.go
+func MinerHeartBeat(key ecdsa.PublicKey, _ignored *bool) (err error) {
+	connectedMiners.Lock()
+	defer connectedMiners.Unlock()
+	k := pubKeyToString(key)
+	if _, ok := connectedMiners.miners[k]; !ok {
+		return err
+	}
+
+	connectedMiners.miners[k].RecentHeartbeat = time.Now().UnixNano()
+
+	return nil
+}
+
+// Deletes dead miners. Adapted from server.go
+func monitor(k string, heartBeatInterval time.Duration) {
+	for {
+		connectedMiners.Lock()
+		if time.Now().UnixNano()-connectedMiners.miners[k].RecentHeartbeat > int64(heartBeatInterval) {
+			fmt.Printf("%s timed out\n", connectedMiners.miners[k].Address.String())
+			delete(connectedMiners.miners, k)
+			connectedMiners.Unlock()
+			return
+		}
+		connectedMiners.Unlock()
+		time.Sleep(heartBeatInterval)
+	}
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// CALLS TO ARTISTS
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// LOCAL
+////////////////////////////////////////////////////////////////////////////////
+func pubKeyToString(key ecdsa.PublicKey) string {
+	return string(elliptic.Marshal(key.Curve, key.X, key.Y))
 }
