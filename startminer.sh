4542488933543fd709b3988b43f9928a50f2c6dd
mrbrendanwong <brendonydwong@gmail.com>
2018-02-15 20:26:26 -0800

    Format start-miner and rename script

--- startminer.sh
+++ startminer.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
-# Generate keys
+#!/bin/bash
+
+# Generate public, private key files
 go run ppkeygen.go
+
 # Read files into env variables
 PRIVKEY=`cat privkey`
 PUBKEY=`cat pubkey`
+
 # Grab server from command line
 SERVER=$1
+
+# Run ink-miner.go
 go run ink-miner.go $SERVER $PUBKEY $PRIVKEY
